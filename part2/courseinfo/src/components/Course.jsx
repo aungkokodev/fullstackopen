9835bf6afb53dd700a9a15e4a3633e0e9fcb7085
aungkokodev <aungkokoepic@gmail.com>
2024-12-08 00:07:11 +0630

    Exercise 2.2-2.3

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,10 +1,14 @@
 import Header from "./Header";
 import Content from "./Content";
+import Total from "./Total";
 
 const Course = ({ course }) => (
   <div>
     <Header name={course.name} />
     <Content parts={course.parts} />
+    <Total
+      total={course.parts.reduce((sum, part) => sum + part.exercises, 0)}
+    />
   </div>
 );
 
