37b835fe873d08bc43d3ebb8d1a27db41e583996
Aung Ko Ko <aungkokoepic@gmail.com>
2025-01-21 17:40:15 +0630

    Exercise 4.4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,5 +1,11 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) return 0
+
+  return blogs.reduce((total, blog) => total + blog.likes, 0)
+}
+
+module.exports = { dummy, totalLikes }
