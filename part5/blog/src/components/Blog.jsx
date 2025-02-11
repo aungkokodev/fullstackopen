0ca83247464b6ee80ec49efd3b0ca6be74fd6d72
aungkokodev <aungkokoepic@gmail.com>
2025-02-11 19:25:00 +0630

    Exercise 5.7

--- part5/blog/src/components/Blog.jsx
+++ part5/blog/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const toggle = () => setShowDetails(!showDetails)
+
+  const buttonLabel = showDetails ? 'hide' : 'view'
+
+  const blogStyle = {
+    border: '1px solid #333',
+    padding: '8px 4px',
+    marginBottom: 8,
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}{' '}
+        <button onClick={toggle}>{buttonLabel}</button>
+      </div>
+      {showDetails && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} <button>like</button>
+          </div>
+          <div>{blog.user.name}</div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
