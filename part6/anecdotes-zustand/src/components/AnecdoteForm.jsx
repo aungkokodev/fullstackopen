7331a020f00a1ffc5cd3929498b6ebf877796b07
aungkokodev <aungkokoepic@gmail.com>
2026-04-16 13:35:53 +0630

    Exercise 6.4

---
+++ part6/anecdotes-zustand/src/components/AnecdoteForm.jsx
@@ -0,0 +1,26 @@
+import { useAnecdoteActions } from '../store'
+
+const AnecdoteForm = () => {
+  const { add } = useAnecdoteActions()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log('submit', event.target.content.value)
+    add(event.target.content.value)
+    event.target.reset()
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input name='content' />
+        </div>
+        <button type='submit'>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
---
+++ part6/anecdotes-zustand/src/components/AnecdoteList.jsx
@@ -0,0 +1,18 @@
+import { useAnecdoteActions, useAnecdotes } from '../store'
+
+const AnecdoteList = () => {
+  const anecdotes = useAnecdotes()
+  const { vote } = useAnecdoteActions()
+
+  return anecdotes.map((anecdote) => (
+    <div key={anecdote.id}>
+      <div>{anecdote.content}</div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={() => vote(anecdote.id)}>vote</button>
+      </div>
+    </div>
+  ))
+}
+
+export default AnecdoteList
